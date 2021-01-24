90ab261771e726d5a29b6aa2d1cea7e499f688e7
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-01-24 16:56:47 -0600

    Edited profile image src

--- src/components/IndexPage/RightSide.jsx
+++ src/components/IndexPage/RightSide.jsx
@@ -9,8 +9,8 @@ const RightSide = () => {
           <img
             src={`${
               isWebpSupported
-                ? "/Images/contact-page-me.webp"
-                : "/Images/contact-page-me.jpg"
+                ? "/images/contact-page-me.webp"
+                : "/images/contact-page-me.jpg"
             }`}
             alt="Rodrigo Terán"
           />
