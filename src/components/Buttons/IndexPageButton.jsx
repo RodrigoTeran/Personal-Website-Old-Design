187ab971f2e8194dec8749246d663b118ad1efec
Rodrigo Teran <rodrigo.teranhernandez@gmail.com>
2021-01-24 15:57:41 -0600

    Added animation to left side index page

--- src/components/Buttons/IndexPageButton.jsx
+++ src/components/Buttons/IndexPageButton.jsx
@@ -1,13 +1,36 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const buttonVariants = {
+  hidden: {
+    y: 150,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      stiffness: 200,
+      type: "spring",
+      mass: 0.4,
+      damping: 8,
+    },
+  },
+};
 
 const IndexPageButton = ({ onClickFunction, text, type }) => {
   return (
-    <button
-      className={`index-page-button ${type === "gradient" ? "gradient" : "blank"}`}
+    <motion.button
+      variants={buttonVariants}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 1.05 }}
+      className={`index-page-button ${
+        type === "gradient" ? "gradient" : "blank"
+      }`}
       onClick={onClickFunction}
     >
       {text}
-    </button>
+    </motion.button>
   );
 };
 export default IndexPageButton;
--- src/components/IndexPage/LeftSide.jsx
+++ src/components/IndexPage/LeftSide.jsx
@@ -1,16 +1,90 @@
 import React from "react";
 import ReactTypingEffect from "react-typing-effect";
 import IndexPageButton from "../Buttons/IndexPageButton";
+import { motion } from "framer-motion";
+
+const sentenceVariants = {
+  hidden: {
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      staggerChildren: 0.1,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const buttonsContainerVariants = {
+  hidden: {
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.1,
+      staggerChildren: 0.3,
+      when: "beforeChildren",
+    },
+  },
+};
+
+const letterVariants = {
+  hidden: {
+    scaleY: 0,
+    originY: "100%",
+    color: "#43f7d5",
+  },
+  visible: {
+    scaleY: 1,
+    originY: "100%",
+    color: "#fff",
+    transition: { type: "spring", stiffness: 200 },
+  },
+};
 
 const LeftSide = () => {
+  const h1Text1 = "Soy Rodrigo";
+  const h1Text2 = "Terán";
   return (
     <div className="index-page-left-side">
       <div className="index-page-left-side-container">
-        <h1>
-          Soy Rodrigo
-          <br />
-          Terán
-        </h1>
+        <motion.h1
+          initial="hidden"
+          animate="visible"
+          variants={sentenceVariants}
+        >
+          <div className="index-page-left-side-div-sentence">
+            {h1Text1.split("").map((char, index) => {
+              return (
+                <motion.div
+                  style={{ marginRight: char === "y" ? "20px" : "" }}
+                  className="index-page-left-side-div-letter"
+                  variants={letterVariants}
+                  key={`${char}-${index}`}
+                >
+                  {char}
+                </motion.div>
+              );
+            })}
+          </div>
+          <div className="index-page-left-side-div-sentence">
+            {h1Text2.split("").map((char, index) => {
+              return (
+                <motion.div
+                  className="index-page-left-side-div-letter"
+                  variants={letterVariants}
+                  key={`${char}-${index}`}
+                  // whileHover="whileHover"
+                >
+                  {char}
+                </motion.div>
+              );
+            })}
+          </div>
+        </motion.h1>
         <h2>
           <ReactTypingEffect
             text={[
@@ -23,7 +97,12 @@ const LeftSide = () => {
             typingDelay={600}
           ></ReactTypingEffect>
         </h2>
-        <div className="index-page-left-side-buttons-container">
+        <motion.div
+          className="index-page-left-side-buttons-container"
+          variants={buttonsContainerVariants}
+          initial="hidden"
+          animate="visible"
+        >
           <IndexPageButton
             onClickFunction={() => {}}
             text="Contáctame"
@@ -34,7 +113,7 @@ const LeftSide = () => {
             text="Portafolio"
             type="noGradient"
           ></IndexPageButton>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
